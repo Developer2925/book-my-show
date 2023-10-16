d402de50abc0c9962767495a0f0acce33d916273
Om Dhakulkar <136986723+Developer2925@users.noreply.github.com>
2023-10-16 19:45:10 +0530

    Delete src directory

--- src/Config/EntertainmentCardCarousal.config.jsx
+++
@@ -1,9 +0,0 @@
-const settings = {
-    infinite: true,
-    autoplay: false,
-    slidesToShow: 5,
-    slidesToScroll: 4,
-    InitialSlide: 0
-}
-
-export default settings;
--- src/Config/PosterCarousalSmMd.config.jsx
+++
@@ -1,9 +0,0 @@
-const settingsMd = {
-    infinite: false,
-    autoplay: false,
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    InitialSlide: 0
-}
-
-export default settingsMd;
